232f31ae677decf8f71a9fbf5f0868f1de36e917
Bahaa Hani <blainefire00@gmail.com>
2023-04-14 16:57:51 +0300

    add car class

--- Blank/src/app/data.service.ts
+++ Blank/src/app/data.service.ts
@@ -5,8 +5,32 @@ import { Injectable } from '@angular/core';
 })
 
 
-export class DataService 
-{
-  constructor( ) 
-  { }
+export class DataService {
+  constructor() { }
+}
+export class Car {
+  type: string;
+  manufacturer: string;
+  model: string;
+  color: string;
+  mileage: number;
+  engine: string;
+  specifications: string;
+  numberOfSeats: number;
+  features: string;
+  price: number;
+  constructor(type: string, manufacturer: string, model: string, color: string, mileage: number, engine: string, specifications: string, numberOfSeats: number, features: string, price: number) {
+    this.type = type;
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.color = color;
+    this.mileage = mileage;
+    this.engine = engine;
+    this.specifications = specifications;
+    this.numberOfSeats = numberOfSeats;
+    this.features = features;
+    this.price = price;
+  }
+
+
 }
