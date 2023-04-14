bb328b9340b30a80e1db04350acd92addd2c62d1
Bahaa Hani <blainefire00@gmail.com>
2023-04-14 16:58:02 +0300

    add class car

--- Blank/src/app/app-routing.module.ts
+++ Blank/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'carview',
+    loadChildren: () => import('./carview/carview.module').then( m => m.CarviewPageModule)
   },
+
 ];
 
 @NgModule({
