ead5508bc3d8d7f6179be6960148de6d464a44cd
bahaa <blainefire00@gmail.com>
2023-04-20 01:57:15 +0300

    tab 1 from may

--- Blank/src/app/auth.service.ts
+++ Blank/src/app/auth.service.ts
@@ -1,9 +1,66 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { AlertController } from '@ionic/angular';
+import { AngularFirestore} from '@angular/fire/compat/firestore';
+import { AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { DocumentReference } from '@angular/fire/compat/firestore';
+import { map, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { NavController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+export interface users{
+  id?:string,
+UserName:string,
+Email:string,
+password:string,
+confirmpass:string
+}
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public user: Observable<users[]>;
+  public userCollection:AngularFirestoreCollection<users>;
 
-  constructor() { }
+  constructor(public s:Storage,public auth:AngularFireAuth,public afs:AngularFirestore) {
+    this.userCollection=this.afs.collection<users>('USERS');
+    this.user=this.userCollection.snapshotChanges().pipe(
+    map(actions=>{
+    return actions.map((a: { payload: { doc: { data: () => any; id: any; }; }; })=>{
+    const data=a.payload.doc.data();
+    const id=a.payload.doc.id;
+    return {id,...data};
+    });
+    })
+    );
+   }
+ngOnInit(): void {}
+
+
+authentication(email:string,password:string){}
+adduser(us:users):Promise<DocumentReference>{
+  return this.userCollection.add(us); 
+  }
+
+  signin(email:string,password:string){
+this.auth.signInWithEmailAndPassword(email,password)
+.then(()=>{alert('login in succssfully')})
+.catch((error: any)=>{alert(error)});
+  }
+signup(email:string,pass:string,user:users){
+  this.auth.createUserWithEmailAndPassword(email,pass)
+    .then(()=>{this.adduser(user).then((response)=>{
+      alert("Inserted Successfully");
+      user={}as users;
+     })
+    }
+    )
+    .catch((erorr: any)=>{alert(erorr)});
+  }
+
+
 }
--- Blank/src/app/tab1/tab1.page.ts
+++ Blank/src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup } from '@angular/forms';
+import { AuthService,users } from '../auth.service';
+import { FormBuilder,Validators } from '@angular/forms';
+
 
 
 @Component({
@@ -7,8 +11,34 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
- 
+ // private USR: Observable<users[]>;
+ public user:users={}as users;
 
-  constructor() {}
-  
+ LoginForm:FormGroup;
+ flag=true;
+ constructor(public fb:FormBuilder,public d:AuthService){ 
+   this.LoginForm=fb.group({
+     userUserName:['', Validators.compose([Validators.required,
+       Validators.pattern('[a-zA-Z0-9]*'), Validators.minLength(5), Validators.maxLength(30)])],
+   
+    Email:['',Validators.compose([Validators.required,
+      Validators.email
+      //pattern('[a-zA-Z0-9._%+-]*+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/')
+    ])],
+    password:['', Validators.compose([Validators.required, Validators.minLength(8),Validators.pattern('[a-zA-Z0-9]*')
+   ,Validators.minLength(8), Validators.maxLength(30)])],
+   cpass: ['', [Validators.required]]
+   });
+ }
+ ngOnInit() {}
+
+ signup(){
+   if(this.LoginForm.valid){
+     this.user.UserName=this.LoginForm.controls['userUserName'].value;
+     this.user.Email=this.LoginForm.controls['Email'].value;
+     this.user.password=this.LoginForm.controls['password'].value;
+     this.user.confirmpass=this.LoginForm.controls['cpass'].value;
+  this.d.signup(this.LoginForm.controls['Email'].value,this.LoginForm.controls['password'].value,this.user);
+}
+}
 }
