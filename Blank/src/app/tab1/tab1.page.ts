1a2b01d3591f77a1244a83bc9921ac163ed0d312
bahaa <blainefire00@gmail.com>
2023-04-21 00:24:00 +0300

    remove tab 1

--- Blank/src/app/tab1/tab1.page.ts
+++ Blank/src/app/tab1/tab1.page.ts
@@ -11,33 +11,5 @@ import { FormBuilder,Validators } from '@angular/forms';
 })
 export class Tab1Page {
  // private USR: Observable<users[]>;
- public user:users={}as users;
-
- LoginForm:FormGroup;
- flag=true;
- constructor(public fb:FormBuilder,public d:AuthService){ 
-   this.LoginForm=fb.group({
-     userUserName:['', Validators.compose([Validators.required,
-       Validators.pattern('[a-zA-Z0-9]*'), Validators.minLength(5), Validators.maxLength(30)])],
-   
-    Email:['',Validators.compose([Validators.required,
-      Validators.email
-      //pattern('[a-zA-Z0-9._%+-]*+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/')
-    ])],
-    password:['', Validators.compose([Validators.required, Validators.minLength(8),Validators.pattern('[a-zA-Z0-9]*')
-   ,Validators.minLength(8), Validators.maxLength(30)])],
-   cpass: ['', [Validators.required]]
-   });
- }
- ngOnInit() {}
-
- signup(){
-   if(this.LoginForm.valid){
-     this.user.UserName=this.LoginForm.controls['userUserName'].value;
-     this.user.Email=this.LoginForm.controls['Email'].value;
-     this.user.password=this.LoginForm.controls['password'].value;
-     this.user.confirmpass=this.LoginForm.controls['cpass'].value;
-  this.d.signup(this.LoginForm.controls['Email'].value,this.LoginForm.controls['password'].value,this.user);
-}
-}
+ 
 }
